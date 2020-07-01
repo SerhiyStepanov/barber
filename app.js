eb7aca278872ae8e2c900bcf5aff3b7d7ee52205
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-07-01 17:27:03 +0300

    ok

---
+++ app.js
@@ -0,0 +1,12 @@
+let car = {
+  color: "red",
+  maxSpeed: 320,
+};
+
+if (car.color == "red") {
+  console.log("qqq");
+} else if (car.color == "green") {
+  console.log("www");
+} else {
+  console.log("xxx");
+}
